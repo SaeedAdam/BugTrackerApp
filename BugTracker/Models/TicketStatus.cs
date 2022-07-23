41ec87937a7a273ba5587d9a130cd72cc3b5b900
Saeed Adam <sandeed70@gmail.com>
2022-07-23 16:42:08 +0300

    Added models

---
+++ BugTracker/Models/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace BugTracker.Models;
+
+public class TicketStatus
+{
+    public int Id { get; set; }
+
+    [DisplayName("Status Name")]
+    public string Name { get; set; }
+}
