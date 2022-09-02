b634158ea47db7fefd06970e0cfceaad5f5c061f
Saeed Adam <sandeed70@gmail.com>
2022-09-02 22:06:58 +0300

    Added style and fixed bugs in Profile page

--- BugTracker/Models/ViewModels/ProfileViewModel.cs
+++ BugTracker/Models/ViewModels/ProfileViewModel.cs
@@ -1,10 +1,11 @@
-﻿namespace BugTracker.Models.ViewModels
+﻿namespace BugTracker.Models.ViewModels;
+
+public class ProfileViewModel
 {
-    public class ProfileViewModel
-    {
-        public Company Company { get; set; }
-        public List<Project> Projects { get; set; }
-        public List<Ticket> Tickets { get; set; }
-        public List<BTUser> Members { get; set; }
-    }
+    public Company Company { get; set; }
+    public List<Project> Projects { get; set; }
+    public List<Project> MyProjects { get; set; }
+    public List<Ticket> Tickets { get; set; }
+    public List<Ticket> MyTickets { get; set; }
+    public List<BTUser> Members { get; set; }
 }
